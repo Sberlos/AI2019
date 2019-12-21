42d8b9efa20d9274e730dc4d83bff4900cecb93c
Francesco Berla <francesco.berla@usi.ch>
2019-12-21 22:22:53 +0100

    Add all methods, some empty. Compiles.
    generatePopulation, TwoOpt and nearestNeighbour are missing. evaluateSol is written but not tested.
    Frees are missing all over the place, only a couple have been added and
    they have not been checked.

--- Cgenetic/genetic.c
+++ Cgenetic/genetic.c
@@ -1,7 +1,18 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+struct Instance {
+    // distance matrix
+    // represented with a single block of memory that I can access using
+    // i * cols + j
+    unsigned * distMatrix;
+    // number of cities
+    unsigned nCities;
+};
 
 struct Population {
-    int size;
+    unsigned size;
     struct Tour ** tours; // array of tours
 };
 
@@ -13,49 +24,183 @@ struct Tour {
     unsigned * solution;
 };
 
-unsigned * gen(unsigned * solution, unsigned populationSize, double mutationRate, unsigned tournamentSize, unsigned elitism, unsigned generations) {
-    struct Population * pop = malloc(sizeof(struct Population));
-    generatePopulation(pop);
-    for (unsigned i = 0; i < generations; ++i) {
-        printf("Migliore: %d", findBest(pop));
-        evolve(pop, mutationRate, elitism);
+unsigned evaluateSol(unsigned * solution, struct Instance * instance) {
+    unsigned length = 0;
+    // I would love to avoid it but I'm not sure how
+    unsigned * temp_sol = malloc(sizeof(unsigned) * instance->nCities + 1);
+    if (!temp_sol) {
+        return 0;
     }
+    memcpy(temp_sol, solution, sizeof(unsigned) * instance->nCities);
+    temp_sol[instance->nCities] = temp_sol[0];
 
-    return getBest(pop)->solution;
+    unsigned from_node = temp_sol[0];
+    
+    for (unsigned i = 1; i <= instance->nCities; ++i) {
+        // not sure about the index of the distMatrix
+        length = length + instance->distMatrix[from_node * instance->nCities + i];
+        // not sure about this either
+        from_node = i;
+    }
+    free(temp_sol);
+    return length;
 }
 
-unsigned * evolve(struct Population * pop, double mutationRate, 
-        unsigned elitism) {
+// TODO implement
+void TwoOpt(struct Tour * tour, unsigned iterations) {
+}
+
+// TODO implement
+unsigned * nearestNeighbour(struct Instance * instance) {
+    unsigned * solution = malloc(sizeof(unsigned) * instance->nCities);
+    return solution;
+}
+
+// TODO implement
+int generatePopulation(struct Population * pop, struct Instance * instance) {
+    return 0;
+}
+
+struct Tour * crossover(struct Tour * p1, struct Tour * p2, unsigned nCities) {
+    // I need to create a new children because the parent could be the one
+    // saved for elitism, therefore modifying it would be bad. And in addition
+    // it could be selected in next rounds of tournament for breeding.
+    struct Tour * child = malloc(sizeof(struct Tour));
+
+    unsigned * permutation = malloc(sizeof(unsigned) * nCities);
+    unsigned * inversion = malloc(sizeof(unsigned) * nCities);
+
+    //int crossoverP1 = rand() % nCities;
+    /* The use of the modulo operator ruin some numbers but I think in the end
+     * it's ok, we will see */
+    int crossoverP1 = rand() % nCities;
+    int crossoverP2 = rand() % nCities;
+
+    // copy all the cities of p1 to the new one
+    memcpy(permutation, p1->solution, sizeof(unsigned) * nCities);
+
+    for (unsigned i = 0; i < nCities; ++i) {
+        inversion[permutation[i]] = i;
+    }
+
+    if (crossoverP1 > crossoverP2) {
+        unsigned temp = crossoverP1;
+        crossoverP1 = crossoverP2;
+        crossoverP2 = temp;
+    }
+
+    for (unsigned i = crossoverP1; i <= crossoverP2; ++i) {
+        unsigned value = p2->solution[i];
+        unsigned invValue = inversion[value];
+        unsigned t = permutation[invValue];
+        permutation[invValue] = permutation[i];
+        permutation[i] = t;
+        t = inversion[permutation[invValue]];
+        inversion[permutation[invValue]] = inversion[permutation[i]];
+        permutation[inversion[i]] = t;
+    }
+
+    child->solution = permutation;
+    // let's calculate the fitness already here, I have to do it somewhere
+    // so it doesn't make any difference
+    // TODO I will need the distance matrix...
+    child->fitness = 0;
+    return child;
+}
+
+void mutate(struct Tour * tour, double mutationRate, unsigned nCities) {
+    unsigned city2;
+    for (unsigned city1 = 0; city1 < nCities; ++city1) {
+        double random = (double)rand() / (double)RAND_MAX;
+        if (random < mutationRate) {
+            city2 = rand() % nCities;
+            while (city2 == city1) {
+                city2 = rand() % nCities;
+            }
+            unsigned temp = tour->solution[city1];
+            tour->solution[city1] = tour->solution[city2];
+            tour->solution[city2] = temp;
+        }
+    }
+}
+
+// TODO check if it's right
+struct Tour * getBest(struct Population * pop) {
+    return pop->tours[0];
+}
+
+struct Tour * TSelection(struct Population * pop) {
+    struct Population * newPop = malloc(sizeof(struct Population));
+    if (!newPop) {
+        return 0;
+    }
+    newPop->size = 5;
+    struct Tour ** tours = malloc(sizeof(struct Tour *) * 5);
+    for (unsigned i = 0; i < newPop->size; ++i) {
+        tours[i] = pop->tours[rand() % pop->size];
+    }
+    free(tours);
+    struct Tour * best = getBest(newPop);
+    free(newPop);
+    return best;
+}
+
+struct Population * evolve(struct Population * pop, double mutationRate, 
+        unsigned elitism, struct Instance * instance) {
     // initialize population
     struct Population * newPopulation = malloc(sizeof(struct Population));
+    if (!newPopulation) {
+        return 0;
+    }
     struct Tour ** newTours = malloc(sizeof(struct Tour *) * pop->size);
+    if (!newTours) {
+        return 0;
+    }
     newPopulation->tours = newTours;
     newPopulation->size = pop->size;
 
     unsigned elitismN = 0;
     if (elitism) {
-        struct Tour * best = findBest(pop);
+        struct Tour * best = getBest(pop);
 
         // not so sure about this line
         // it's the first one, so maybe it's like this
-        newPopulation->tours = best;
+        newPopulation->tours[0] = best;
         elitismN = 1;
     }
 
     for (unsigned i = elitismN; i < newPopulation->size; ++i) {
         struct Tour * parent1 = TSelection(pop);
         struct Tour * parent2 = TSelection(pop);
-        struct Tour * child = crossover(parent1, parent2);
-        mutate(child, mutationRate);
+        // nCities is going to be moved to instance
+        struct Tour * child = crossover(parent1, parent2, instance->nCities);
+        mutate(child, mutationRate, instance->nCities);
 
         TwoOpt(child, 15);
-        /* this is the first option
-        struct Tour * position = newPopulation->tours + i;
-        position = childM;
-        */
-        // second option, is it fully correct?
-        newPopulation->tours[i] = childM;
+        newPopulation->tours[i] = child;
     }
 
+    // I have to add a loop to free everything that's not in the new
+    // generation...
+
     return newPopulation;
+}
+
+unsigned * gen(unsigned * solution, unsigned populationSize, double mutationRate, unsigned tournamentSize, unsigned elitism, unsigned generations, struct Instance * instance) {
+    // command to set the seed of the random function
+    // srand(seed);
+    // srandom(seed); // It should be Linux specific
+    struct Population * pop = malloc(sizeof(struct Population));
+    generatePopulation(pop, instance);
+    for (unsigned i = 0; i < generations; ++i) {
+        printf("Migliore: %d", getBest(pop)->fitness);
+        pop = evolve(pop, mutationRate, elitism, instance);
+    }
+
+    struct Tour * t = getBest(pop);
+    return t->solution;
+    //return getBest(pop)->solution;
+}
+
+int main() {
 }
