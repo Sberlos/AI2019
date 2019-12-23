591b0c68318af16d8d8cc14adf3c4179fa866097
Francesco Berla <francesco.berla@usi.ch>
2019-12-23 02:56:04 +0100

    Add problem initializations and checks
    Add the function for reading from a file and the creation of the
    distance matrix and the instance.
    Add some malloc checks.

--- Cgenetic/genetic.c
+++ Cgenetic/genetic.c
@@ -1,6 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
+
+struct City {
+    unsigned id;
+    double x;
+    double y;
+};
 
 struct Instance {
     // distance matrix
@@ -9,6 +16,8 @@ struct Instance {
     unsigned * distMatrix;
     // number of cities
     unsigned nCities;
+
+    unsigned bestKnown;
 };
 
 struct Population {
@@ -17,7 +26,7 @@ struct Population {
 };
 
 struct Tour {
-    /* se lo tengo come int (usando la differenza di lunghezza tra il migliore 
+    /* se lo tengo come int (usando la differenza di lunghezza tra il migliore
      * e questo) dovrei guadagnare in performance
      */
     double fitness; //could change to unsigned with some tricks maybe
@@ -35,7 +44,7 @@ unsigned evaluateSol(unsigned * solution, struct Instance * instance) {
     temp_sol[instance->nCities] = temp_sol[0];
 
     unsigned from_node = temp_sol[0];
-    
+
     for (unsigned i = 1; i <= instance->nCities; ++i) {
         // not sure about the index of the distMatrix
         length = length + instance->distMatrix[from_node * instance->nCities + i];
@@ -53,6 +62,9 @@ void TwoOpt(struct Tour * tour, unsigned iterations) {
 // TODO implement
 unsigned * nearestNeighbour(struct Instance * instance) {
     unsigned * solution = malloc(sizeof(unsigned) * instance->nCities);
+    if (!solution) {
+        return 0;
+    }
     return solution;
 }
 
@@ -66,9 +78,18 @@ struct Tour * crossover(struct Tour * p1, struct Tour * p2, unsigned nCities) {
     // saved for elitism, therefore modifying it would be bad. And in addition
     // it could be selected in next rounds of tournament for breeding.
     struct Tour * child = malloc(sizeof(struct Tour));
+    if (!child) {
+        return 0;
+    }
 
     unsigned * permutation = malloc(sizeof(unsigned) * nCities);
+    if (!permutation) {
+        return 0;
+    }
     unsigned * inversion = malloc(sizeof(unsigned) * nCities);
+    if (!inversion) {
+        return 0;
+    }
 
     //int crossoverP1 = rand() % nCities;
     /* The use of the modulo operator ruin some numbers but I think in the end
@@ -101,7 +122,7 @@ struct Tour * crossover(struct Tour * p1, struct Tour * p2, unsigned nCities) {
     }
 
     child->solution = permutation;
-    
+
     // don't calculate the fitness here as I have to mutate and do local
     // search afterwards and I would have to recalculate it
     child->fitness = 0;
@@ -124,11 +145,12 @@ void mutate(struct Tour * tour, double mutationRate, unsigned nCities) {
     }
 }
 
-// TODO check if it's right
+// TODO implement
 struct Tour * getBest(struct Population * pop) {
     return pop->tours[0];
 }
 
+// TODO check if it's right
 struct Tour * TSelection(struct Population * pop) {
     struct Population * newPop = malloc(sizeof(struct Population));
     if (!newPop) {
@@ -136,6 +158,9 @@ struct Tour * TSelection(struct Population * pop) {
     }
     newPop->size = 5;
     struct Tour ** tours = malloc(sizeof(struct Tour *) * 5);
+    if (!tours) {
+        return 0;
+    }
     for (unsigned i = 0; i < newPop->size; ++i) {
         tours[i] = pop->tours[rand() % pop->size];
     }
@@ -145,7 +170,7 @@ struct Tour * TSelection(struct Population * pop) {
     return best;
 }
 
-struct Population * evolve(struct Population * pop, double mutationRate, 
+struct Population * evolve(struct Population * pop, double mutationRate,
         unsigned elitism, struct Instance * instance) {
     // initialize population
     struct Population * newPopulation = malloc(sizeof(struct Population));
@@ -180,8 +205,6 @@ struct Population * evolve(struct Population * pop, double mutationRate,
         newPopulation->tours[i] = child;
     }
 
-    // I have to add a loop to free everything that's not in the new
-    // generation...
     // TODO check if it's right
     for (unsigned i = 0; i < newPopulation->size; ++i) {
         if (pop->tours[i] != newPopulation->tours[0]) {
@@ -194,11 +217,16 @@ struct Population * evolve(struct Population * pop, double mutationRate,
     return newPopulation;
 }
 
-unsigned * gen(unsigned * solution, unsigned populationSize, double mutationRate, unsigned tournamentSize, unsigned elitism, unsigned generations, struct Instance * instance) {
+unsigned * gen(unsigned * solution, unsigned populationSize, double mutationRate,
+        unsigned tournamentSize, unsigned elitism, unsigned generations,
+        struct Instance * instance) {
     // command to set the seed of the random function
     // srand(seed);
     // srandom(seed); // It should be Linux specific
     struct Population * pop = malloc(sizeof(struct Population));
+    if (!pop) {
+        return 0;
+    }
     generatePopulation(pop, instance);
     for (unsigned i = 0; i < generations; ++i) {
         printf("Migliore: %f", getBest(pop)->fitness);
@@ -211,9 +239,91 @@ unsigned * gen(unsigned * solution, unsigned populationSize, double mutationRate
     //return getBest(pop)->solution;
 }
 
+unsigned * createDistMaxtrix(struct Instance * instance, struct City ** cities) {
+    unsigned nCities = instance->nCities;
+    unsigned * distMatrix =
+        malloc(sizeof(unsigned) * nCities * nCities);
+    if (!distMatrix) {
+        return 0;
+    }
+    for (unsigned i = 0; i < nCities; ++i) {
+        for (unsigned j = 0; j < nCities; ++j) {
+            if (i == j) {
+                distMatrix[i * nCities + j] = 0;
+            } else {
+                distMatrix[i * nCities + j] = 
+                    round(sqrt(pow(cities[i]->x - cities[j]->x, 2.0) + 
+                                pow(cities[i]->y - cities[j]->y, 2.0)));
+                distMatrix[j * nCities + i] = distMatrix[i * nCities + j];
+            }
+        }
+    }
+    return distMatrix;
+}
+
+struct Instance * readProblem(char ** argv) {
+    struct Instance * instance = malloc(sizeof(struct Instance));
+    if (!instance) {
+        return 0;
+    }
+    char line[50];
+    unsigned i = 0;
+    char[20] path;
+    // TODO check if it's the right path
+    strcpy(path, "../problems/");
+    strcpy(path, argv[1]);
+    FILE* file = fopen(path, "r");
+    struct City * city = 0;
+    struct City ** cities = 0;
+    while (fgets(line, 50, file)) {
+        if (!strcmp(line, "EOF")) {
+            break;
+        }
+        if (i == 3) {
+            scanf("DIMENSION : %d", &(instance->nCities));
+            cities = malloc(sizeof(struct City *) * instance->nCities);
+            if (!cities) {
+                return 0;
+            }
+        }
+        if (i == 5) {
+            scanf("BEST_KNOWN : %d", &(instance->bestKnown));
+        }
+        if (i > 6) {
+            city = malloc(sizeof(struct City));
+            if (!city) {
+                for (unsigned j = 7; j < i; ++j) {
+                    free(cities[j]);
+                }
+                return 0;
+            }
+            scanf("%u %lf %lf", &(city->id), &(city->x), &(city->y));
+            cities[i-7] = city;
+        }
+        ++i;
+    }
+    createDistMaxtrix(instance, cities);
+    for (unsigned i = 0; i < instance->nCities; ++i) {
+        free(cities[i]);
+    }
+    free(cities);
+    fclose(file);
+    return instance;
+}
+
+void checksolution(unsigned * sol) {
+}
+
 int main(int argc, char **argv) {
+    printf("%f", pow(2, 3));
     if (argc < 2) {
         return 1;
     }
-    // TODO call method with argv
+    struct Instance * instance = readProblem(argv);
+    unsigned * solution = nearestNeighbour(instance);
+    solution = gen(solution, 30, 0.015, 5, 1, 10, instance);
+    checksolution(solution);
+    free(instance->distMatrix);
+    free(instance);
+    free(solution);
 }
